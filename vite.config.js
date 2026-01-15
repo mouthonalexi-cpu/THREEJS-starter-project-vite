c07f29a49b9587902a4120548423f962d66cd1b8
mouthonalexi-cpu <mouthonalexi@gmail.com>
2026-01-15 23:46:49 +0100

    beta 2.2

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
-import { resolve } from "path";
-
+import { resolve } from "node:path";
 
 export default defineConfig({
+  plugins: [glsl()],
+
   build: {
     rollupOptions: {
       input: {
         index: resolve(__dirname, "index.html"),
-        produit: resolve(__dirname, "produit-selec.html"),
         boutique: resolve(__dirname, "boutique.html"),
         publication: resolve(__dirname, "publication.html"),
-        produit: resolve(__dirname, "produit-PTC01.html"),
-        produit: resolve(__dirname, "produit-PTC02.html"),
+
+        produitSelect: resolve(__dirname, "Produit-selec.html"),
+        produitPBC01: resolve(__dirname, "produit-PBC01.html"),
+        produitPBC02: resolve(__dirname, "produit-PBC02.html"),
+        produitPTC01: resolve(__dirname, "produit-PTC01.html"),
+        produitPTC02: resolve(__dirname, "produit-PTC02.html"),
       },
     },
   },
